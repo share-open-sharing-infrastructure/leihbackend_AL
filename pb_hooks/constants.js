c028e811fae99d54a2650665f33b3700735d9bdc
I504010 <me@timojohner.de>
2026-05-01 14:02:03 +0200

    disable dry mode

--- pb_hooks/constants.js
+++ pb_hooks/constants.js
@@ -22,7 +22,7 @@ const DELETION_GRACE_PERIOD_DAYS = $os.getenv('LL_DELETION_GRACE_PERIOD_DAYS')
 const NO_WELCOME = $os.getenv('LL_NO_WELCOME') === 'true'
 const NO_DELETE_INACTIVE = $os.getenv('LL_NO_DELETE_INACTIVE') === 'true'
 const LOOPS_API_KEY = $os.getenv('LOOPS_API_KEY') || ''
-const DRY_MODE = $os.getenv('DRY_MODE') !== 'false'
+const DRY_MODE = false
 const IMPORT_MODE = $os.getenv('IMPORT_MODE') === 'true'
 const LOG_LEVEL = parseInt($os.getenv('LOG_LEVEL') || '4')  // warning
 
